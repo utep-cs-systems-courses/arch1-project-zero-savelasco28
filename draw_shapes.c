422c3099cf0d94085ea5921ae972e81eb97dc1bb
savelasco28 <savelasco@miners.utep.edu>
2022-09-12 11:15:16 -0600

    Changes on 9/12

--- draw_shapes.c
+++ draw_shapes.c
@@ -26,3 +26,23 @@ void print_triangle(int leftCol, int size)
   }
 }
 
+//Prints an arrow
+void print_arrow(int leftCol, int size)
+{
+  for(int row =0; row <= size;row++){
+    int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+    int col;
+    for(col = 0; col < minCol; col++)putchar(' ');
+    for(; col <= maxCol;col++)putchar('*');
+    putchar('\n');
+  }
+
+  int subSize = size / 3;
+  for(int row = 0; row <= size; row++){
+    int minCol = leftCol + size - subSize, maxCol = leftCol + size + subSize;
+    int col;
+    for(col = 0; col < minCol; col++)putchar(' ');
+    for(; col<= maxCol; col++)putchar('*');
+    putchar('\n');
+  }
+}
